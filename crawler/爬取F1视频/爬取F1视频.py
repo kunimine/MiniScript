1263a734dec4ffd6b2165b84bb19baab479cd1ef
henaer <henaer@tom.com>
2022-12-25 08:43:35 +0800

    202221225

---
+++ crawler/爬取F1视频/爬取F1视频.py
@@ -0,0 +1,15 @@
+import requests
+import pandas as pd
+
+# r = requests.get()
+
+urlx = "http://"+ urlInput + ".com"
+urlInput = input("请输入网站")
+
+df = pd.to_excel('C:\Users\henae\OneDrive\HeNaerX\kunimine\MiniScript\crawler\爬取F1视频\data.xlsx:')
+
+
+
+# 将url保存到本地数据库供下次使用
+
+print(urlx)
---
+++ 给每个文件添加表头.py
@@ -0,0 +1,7 @@
+import os
+
+firstLine = "TAME:" + today + nowtime
+twoLine = ""
+threeLine = "本文件归赫纳尔科技有限公司所有"
+fourLine = "HeNaer Inc"
+
